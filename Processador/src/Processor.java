f84db47638482b30fa175fa929a04626246b2917
spraxx <45296009+spraxx@users.noreply.github.com>
2021-11-22 14:28:46 +0000

    Add files via upload

---
+++ Processador/src/Processor.java
@@ -0,0 +1,30 @@
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Processor {
+
+    public static void main(String args[]) throws RemoteException {
+
+        pRequestManager pReqMan = new pRequestManager();
+        Registry r = null;
+
+        try{
+            r= LocateRegistry.createRegistry(2020);
+            thread1 m1 = new thread1();
+            Thread t1 = new Thread(m1); // Using the constructor Thread(Runnable r)
+            t1.start();
+        }
+        catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+        try{
+            r.rebind("pRequestManager", pReqMan);
+            System.out.println("Processador a correr!");
+        }
+        catch (Exception e) {
+            System.out.println("Processador principal " + e.getMessage());
+        }
+    }
+}
