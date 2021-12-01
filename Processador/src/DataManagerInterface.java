2aec98ae775496541f2c25c0f24439955595d5c2
spraxx <45296009+spraxx@users.noreply.github.com>
2021-12-01 20:28:04 +0000

    Delete Processador directory

--- Processador/src/DataManagerInterface.java
+++
@@ -1,7 +0,0 @@
-import java.io.IOException;
-import java.rmi.Remote;
-
-public interface DataManagerInterface extends Remote {
-
-    LoaderAnswer execRequest(ProcessRequest pRequest) throws IOException;
-}
