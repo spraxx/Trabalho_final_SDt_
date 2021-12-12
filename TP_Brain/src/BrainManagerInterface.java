2d503f9055a4a9913d6e7862a39f4db6d0c9f2a6
spraxx <45296009+spraxx@users.noreply.github.com>
2021-12-12 22:40:59 +0000

    Delete TP_Brain directory

--- TP_Brain/src/BrainManagerInterface.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface BrainManagerInterface extends Remote {
-    void saveModel(BrainModel model) throws RemoteException;
-    BrainModel getModel(String id) throws RemoteException;
-}
