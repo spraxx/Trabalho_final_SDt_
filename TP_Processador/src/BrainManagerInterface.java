869371558a50f67a9904340847388c51b4697932
spraxx <45296009+spraxx@users.noreply.github.com>
2022-02-18 15:44:17 +0000

    Delete TP_Processador directory

--- TP_Processador/src/BrainManagerInterface.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface BrainManagerInterface extends Remote {
-    void NewModel(String model1, String procID, String pID) throws RemoteException;
-    public String ModelRequest(String pID) throws RemoteException;
-}
